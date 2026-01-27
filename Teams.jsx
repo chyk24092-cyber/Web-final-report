57e9de03d670cff24e66532407335e8031d94a72
chyk24092-cyber <chyk24092@g.nihon-u.ac.jp>
2026-01-27 16:05:14 +0900

    1/27 16.5

--- Teams.jsx
+++ Teams.jsx
@@ -8,12 +8,12 @@ import { getFirestore, collection, onSnapshot, doc, setDoc, updateDoc, getDoc } 
 
 // 環境変数から読み込む
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FB_API_KEY,
-  authDomain: import.meta.env.VITE_FB_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FB_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FB_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FB_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FB_APP_ID
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 
 // Firebaseに接続
